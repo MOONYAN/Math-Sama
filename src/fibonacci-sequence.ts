f0bb329d97239c20f7c1077cd1a47a1ee46bc834
MOONYAN <c8763sasuke@ntut.org.tw>
2021-05-11 23:23:50 +0800

    update fibonacci-sequence

--- src/fibonacci-sequence.ts
+++ src/fibonacci-sequence.ts
@@ -21,30 +21,12 @@ export function compose(A: Mat, B: Mat): Mat {
 }
 
 export function eigenvalues(A: Mat): number[] {
-
-    const findLambdas = (a: number, b: number, c: number): number[] => {
-        const d: number = (b ** 2 - 4 * a * c) ** 0.5;
-        return [
-            (-b + d) / (2 * a),
-            (-b - d) / (2 * a)
-        ];
-    }
-
-    return findLambdas(1, -A[0][0], - A[0][1] * A[1][0]);
-}
-
-export function eigenvector(A: Mat, eigenvalue: number): Vec {
+    const h = A[0][0];
+    const k = A[1][0];
+    const d = (h ** 2 + 4 * k) ** 0.5;
     return [
-        -A[1][0] / (A[0][0] - eigenvalue),
-        1
-    ];
-}
-
-export function inverse(A: Mat): Mat {
-    const det = A[0][0] * A[1][1] - A[0][1] * A[1][0];
-    return [
-        [A[1][1] / det, -A[0][1] / det],
-        [-A[1][0] / det, A[0][0] / det]
+        (h + d) / 2,
+        (h - d) / 2
     ]
 }
 
@@ -74,10 +56,14 @@ export function fibonacciSequence(A: Mat, f0: number, f1: number): Func {
         [0, lambdas[1]]
     ]
     const P: Mat = [
-        eigenvector(A, lambdas[0]),
-        eigenvector(A, lambdas[1])
+        [lambdas[0], 1],
+        [lambdas[1], 1]
     ]
-    const PInverse: Mat = inverse(P);
+    const detP = lambdas[0] - lambdas[1];
+    const PInverse: Mat = [
+        [1 / detP, -1 / detP],
+        [-lambdas[1] / detP, lambdas[0] / detP]
+    ]
 
     return (n: number): number => {
         const Dn: Mat = diagonalPow(D, n - 1);
